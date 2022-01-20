01d0cab65c166418f1d80196f17a957f1e147271
Nick <nick.skelin@gmail.com>
2022-01-20 02:35:49 -0800

    Properly centered skill cards titles

--- components/skillCard.jsx
+++ components/skillCard.jsx
@@ -7,7 +7,9 @@ export default function SkillCard({imageSource, title, skills}) {
 	return (
 		<div className={styles.skillCard}>
 			<Image src={imageSource} alt="Icon to represent the skill" />
-			<b>{title}</b>
+			<div className={styles.title}>
+				<b>{title}</b>
+			</div>
 			<hr className={styles.hr} />
 			<ul className={styles.skillCardUl}>{skillsElement}</ul>
 		</div>
