c63140697ec28a0487c2d9fcf8232c9ebc9cee49
Nick <nick.skelin@gmail.com>
2022-01-22 02:44:09 -0800

    renamed classname to match new component name

--- components/hyperLink.jsx
+++ components/hyperLink.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/hyperLink.module.css";
 
 export default function HyperLink({text, icon, icon: {source, height, alt, width}, href}) {
 	return (
-		<div className={styles.hyperLinkWithIcon}>
+		<div className={styles.hyperLink}>
 			{icon && <Image src={source} width={width} height={height} alt={alt} />}
 			<a href={href}>
 				<span className={styles.text}>{text}</span>
