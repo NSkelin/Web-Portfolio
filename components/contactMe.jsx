5e13bc9923b1f145785c3f7940bdf55e0b203d5b
Nick <nick.skelin@gmail.com>
2022-01-22 18:00:03 -0800

    changed unique key from text to index as text is optional

--- components/contactMe.jsx
+++ components/contactMe.jsx
@@ -3,8 +3,8 @@ import LineTitle from "../components/lineTitle";
 import HyperLink from "../components/hyperLink";
 
 export default function ContactMe({title, description, links}) {
-	const linkElement = links.map(({text, icon, href}) => {
-		return <HyperLink key={text} text={text} icon={icon} href={href} />;
+	const linkElement = links.map(({text, icon, href}, index) => {
+		return <HyperLink key={index} text={text} icon={icon} href={href} />;
 	});
 
 	return (
