54c690fe7c8933404433019c6da4cc9c791c3f93
Nick <nick.skelin@gmail.com>
2022-01-22 17:59:21 -0800

    updated data to follow type warning

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,14 +42,14 @@ const linkCarder = {
 };
 
 const contactLinks = [
-    {text: "nick.skelin@gmail.com", icon: {source: emailIcon, height: "35", width: "35", alt: "Icon"}},
-    {text: "Github", icon: {source: githubIcon, height: "35", width: "35", alt: "Icon"}, href: "https://github.com/NSkelin"},
+    {text: "nick.skelin@gmail.com", icon: {source: emailIcon, height: 35, width: 35, alt: "Icon"}},
+    {text: "Github", icon: {source: githubIcon, height: 35, width: 35, alt: "Icon"}, href: "https://github.com/NSkelin"},
     {
         text: "LinkedIn",
         icon: {
             source: linkedInIcon,
-            height: "35",
-            width: "35",
+            height: 35,
+            width: 35,
             alt: "Icon",
         },
         href: "https://www.linkedin.com/in/nicholas-skelin-401014173/",
