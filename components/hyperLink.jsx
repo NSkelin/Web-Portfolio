a8abab15c7725bc6691a5eea4532b6161d90b6c3
Nick <nick.skelin@gmail.com>
2022-01-22 17:38:22 -0800

    Added type checking and default values

--- components/hyperLink.jsx
+++ components/hyperLink.jsx
@@ -1,13 +1,27 @@
 import Image from "next/image";
 import styles from "../styles/hyperLink.module.css";
+import PropTypes from "prop-types";
 
-export default function HyperLink({text, icon, icon: {source, height, alt, width}, href}) {
+function HyperLink({text = "link", icon: {source, height = 35, alt = "icon", width = 35}, href}) {
 	return (
 		<div className={styles.container}>
 			<a className={styles.link} href={href}>
-				{icon && <Image src={source} width={width} height={height} alt={alt} />}
+				{source && <Image src={source} width={width} height={height} alt={alt} />}
 				<span className={styles.text}>{text}</span>
 			</a>
 		</div>
 	);
 }
+
+HyperLink.propTypes = {
+	text: PropTypes.string,
+	href: PropTypes.string.isRequired,
+	icon: PropTypes.shape({
+		source: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+		height: PropTypes.number,
+		width: PropTypes.number,
+		alt: PropTypes.string,
+	}),
+};
+
+export default HyperLink;
