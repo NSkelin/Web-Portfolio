b53b0a729267509c583e6ea789ebdee2f1be5a98
Nick <nick.skelin@gmail.com>
2022-02-17 19:33:29 -0800

    fix incorrect casing for html element

--- components/contactMe.jsx
+++ components/contactMe.jsx
@@ -12,7 +12,7 @@ function ContactMe({links}) {
 			<form className={styles.form}>
 				<h2>Send a message</h2>
 				<input className={styles.input} placeholder="Your email (so i can respond)."></input>
-				<textArea className={styles.textArea} rows="7" placeholder="Your message goes here."></textArea>
+				<textarea className={styles.textArea} rows="7" placeholder="Your message goes here."></textarea>
 				<button className={styles.button}>Submit</button>
 			</form>
 			<div className={styles.links}>
