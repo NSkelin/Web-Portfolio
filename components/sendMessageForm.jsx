157d6191d824a3be102c89e79913892639d20171
Nick <nick.skelin@gmail.com>
2022-11-14 01:27:10 -0800

    Add basic input validation on submit
    Checks string character count on inputs.

--- components/sendMessageForm.jsx
+++ components/sendMessageForm.jsx
@@ -12,8 +12,35 @@ function SendMessageForm() {
 	let summaryMessage = "Message sent!";
 	let summaryError = false;
 
+	async function handleSubmit(e) {
+		e.preventDefault();
+		verifyInput();
+	}
+
+	function verifyInput() {
+		// check email input
+		console.log("t");
+		if (email.length === 0) {
+			console.log("t1");
+			emailIncorrect = true;
+			emailError = "Email cannot be blank.";
+		} else {
+			emailIncorrect = false;
+			emailError = "";
+		}
+
+		// check message input
+		if (!userMessage.length >= 50) {
+			messageIncorrect = true;
+			messageError = "Message must be more than 50 characters.";
+		} else {
+			messageIncorrect = false;
+			messageError = "";
+		}
+	}
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit}>
 			<h2>Send a message</h2>
 			<input
 				className={emailIncorrect === true ? styles.inputError : styles.input}
