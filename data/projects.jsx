e0e483feb5a299c146a2c5c6b7e15f5507f06e59
Nick <nick.skelin@gmail.com>
2023-03-14 13:51:21 -0700

    add projects to portfolio

--- data/projects.jsx
+++ data/projects.jsx
@@ -1,7 +1,22 @@
+import D2StatMapImage from "../public/D2StatMap.png";
 import PortfolioImage from "../public/Portfolio.png";
+import SpotBotImage from "../public/Spotbot.png";
+import CartCompanionImage from "../public/CartCompanion.png";
+import EdifyLearningSpacesImage from "../public/EdifyLearningSpaces.png";
 
 const ProjectData = {
 	cards: [
+		{
+			cardType: "link",
+			title: "D2StatMap",
+			imageSource: D2StatMapImage,
+			array: [
+				{
+					icon: {source: "akar-icons:github-fill", width: 40, height: 40, color: "white"},
+					link: "https://github.com/NSkelin/d2statmap",
+				},
+			],
+		},
 		{
 			cardType: "link",
 			title: "Portfolio",
@@ -10,6 +25,39 @@ const ProjectData = {
 				{
 					icon: {source: "akar-icons:github-fill", width: 40, height: 40, color: "white"},
 					link: "https://github.com/NSkelin/Web-Portfolio",
+				},
+			],
+		},
+		{
+			cardType: "link",
+			title: "SpotBot",
+			imageSource: SpotBotImage,
+			array: [
+				{
+					icon: {source: "akar-icons:github-fill", width: 40, height: 40, color: "white"},
+					link: "https://github.com/NSkelin/spotbot",
+				},
+			],
+		},
+		{
+			cardType: "link",
+			title: "CartCompanion",
+			imageSource: CartCompanionImage,
+			array: [
+				{
+					icon: {source: "akar-icons:github-fill", width: 40, height: 40, color: "white"},
+					link: "https://github.com/NSkelin/Quicklists",
+				},
+			],
+		},
+		{
+			cardType: "link",
+			title: "Edify Learning Spaces",
+			imageSource: EdifyLearningSpacesImage,
+			array: [
+				{
+					icon: {source: "akar-icons:github-fill", width: 40, height: 40, color: "white"},
+					link: "https://github.com/NSkelin/EdifyLearningSpacesWebApp",
 				},
 			],
 		},
