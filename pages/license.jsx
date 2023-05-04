48e64ff345a651e48bda653f5dbdf9b90858b94f
Nick <nick.skelin@gmail.com>
2023-05-04 13:21:44 -0700

    fix using nextjs Head

--- pages/license.jsx
+++ pages/license.jsx
@@ -1,7 +1,8 @@
+import Head from "next/head";
 export default function License() {
 	return (
 		<div className="container">
-			<head></head>
+			<Head></Head>
 			<main>licensing goes here</main>
 			<footer></footer>
 		</div>
