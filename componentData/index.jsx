17d5bab70a6ef1770f45e203fcf5dbc17d941c5b
Nick <nick.skelin@gmail.com>
2023-05-12 16:46:42 -0700

    add export

--- componentData/index.jsx
+++ componentData/index.jsx
@@ -1,3 +1,4 @@
 export {default as projectsData} from "./projectsData";
 export {default as aboutData} from "./aboutData";
 export {default as contactData} from "./contactData";
+export {default as landingData} from "./landingData";
