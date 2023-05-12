0f93366baa60de787f3ecaa1dc1bbac811861b74
Nick <nick.skelin@gmail.com>
2023-05-12 16:46:30 -0700

    add file

---
+++ componentData/landingData.jsx
@@ -0,0 +1,9 @@
+import pfp from "../public/profile_picture.png";
+
+const landingData = {
+	image: pfp,
+	title: "Hey, im Nick!",
+	subtitle: "I develop websites.",
+};
+
+export default landingData;
