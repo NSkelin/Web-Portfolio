f556d4df283267f6482f51024233432db34ac7a0
Nick <nick.skelin@gmail.com>
2023-05-30 15:13:00 -0700

    update css import to scss

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./About.module.css";
+import styles from "./About.module.scss";
 import AccentTitle from "../AccentTitle/AccentTitle";
 import Image from "next/image";
 import {ImageProps} from "next/image";
