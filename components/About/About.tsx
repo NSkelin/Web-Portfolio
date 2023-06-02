7881600c6f740da66a8e43d3b6e11b11d89fa07c
Nick <nick.skelin@gmail.com>
2023-06-01 18:12:53 -0700

    refactor component simplified html and moved it around for css

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./About.module.scss";
-import AccentTitle from "../AccentTitle/AccentTitle";
 import Image from "next/image";
 import {ImageProps} from "next/image";
 
@@ -11,12 +10,7 @@ export type AboutProps = {description: string[]; interests: Interest[]; skillIco
 function createInterestList(interests: Interest[]) {
 	const interestList = interests.map((interest, index) => {
 		if (Array.isArray(interest)) {
-			return (
-				<ul className={styles.list} key={index}>
-					{" "}
-					{createInterestList(interest)}{" "}
-				</ul>
-			);
+			return <ul key={index}> {createInterestList(interest)} </ul>;
 		} else {
 			return <li key={index}>{interest}</li>;
 		}
@@ -28,7 +22,7 @@ function createInterestList(interests: Interest[]) {
 function createSkillRows(skillRowsData: Skill[][]) {
 	const skillRowsArr = skillRowsData.map((arr, index) => {
 		return (
-			<ul key={index} className={styles.skillRow}>
+			<ul key={index} className={styles.row}>
 				{createSkillIcons(arr)}
 			</ul>
 		);
@@ -40,7 +34,7 @@ function createSkillIcons(skills: Skill[]) {
 	const skillIcons = skills.map(({Icon, text}, index) => {
 		return (
 			<li key={index} className={styles.skillIconGroup}>
-				<Icon className={styles.skillIcon} />
+				<Icon className={styles.icon} />
 				<p>{text}</p>
 			</li>
 		);
@@ -51,41 +45,29 @@ function createSkillIcons(skills: Skill[]) {
 function About({description, interests, skillIconSources, image: {src, alt}}: AboutProps) {
 	const interestList = createInterestList(interests);
 	const skillIcons = createSkillRows(skillIconSources);
-	const text = description.map((paragraph, index) => (
-		<p className={styles.p} key={index}>
-			{paragraph}
-		</p>
-	));
+	const descriptionText = description.map((paragraph, index) => <p key={index}>{paragraph}</p>);
 
 	return (
 		<section className={styles.about}>
-			<div className={styles.topRow}>
-				<div className={styles.aboutTitleMobile}>
-					<AccentTitle title={"About me"} />
-					<Image className={styles.pfp} src={src} alt={alt} />
-					<div className={styles.description}>{text}</div>
+			<section className={styles.personal}>
+				<div className={styles.imageWrapper}>
+					<Image className={styles.pfp} src={src} alt={alt} sizes="(max-width: 600px) 172px, 232px" />
 				</div>
-				<div className={styles.aboutTitleDesktop}>
-					<Image className={styles.pfp} src={src} alt={alt} />
-					<div className={styles.description}>
-						<AccentTitle title={"About me"} />
-						{text}
-					</div>
-				</div>
-			</div>
+				<h2>{"About"}</h2>
+				<div className={styles.divider}></div>
+				{descriptionText}
+			</section>
 
-			<div className={styles.bottomRow}>
-				<section className={styles.skills}>
-					<h3 className={styles.title}>Skills</h3>
-					<div className={styles.skillRows}>{skillIcons}</div>
-				</section>
-				<aside className={styles.interests}>
-					<h3 className={styles.title}>Interests</h3>
-					<div className={styles.interestContent}>
-						<ul className={styles.list}>{interestList}</ul>
-					</div>
-				</aside>
-			</div>
+			<aside className={styles.interests}>
+				<div className={styles.divider}></div>
+				<h3>{"Interests"}</h3>
+				<ul>{interestList}</ul>
+			</aside>
+
+			<section className={styles.skills}>
+				<h3 className={styles.title}>Skills</h3>
+				{skillIcons}
+			</section>
 		</section>
 	);
 }
