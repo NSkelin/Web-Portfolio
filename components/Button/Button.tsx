0322465bbb014b292740b29bd2ecd197d881a287
Nick <nick.skelin@gmail.com>
2023-06-06 17:57:03 -0700

    add type to onclick property

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,13 @@
-import React, {ReactNode} from "react";
+import React, {MouseEventHandler, ReactNode} from "react";
 import styles from "./Button.module.scss";
 
+type ButtonProps = {
+	children?: ReactNode;
+	style: ButtonStyles;
+	onClick: MouseEventHandler<HTMLButtonElement>;
+};
 type ButtonStyles = "fab" | "filled" | "outlined";
-function Button({children, style, onClick}: {children?: ReactNode; style: ButtonStyles; onClick: any}) {
+function Button({children, style, onClick}: ButtonProps) {
 	return (
 		<button onClick={onClick} className={styles[style]}>
 			{children}
