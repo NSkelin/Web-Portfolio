209e14dc8fbd5756a4179630e99e8d90d167efe1
Nick <nick.skelin@gmail.com>
2023-06-14 13:41:17 -0700

    add transition when expanding / collapsing

--- components/CollapsibleArea/CollapsibleArea.tsx
+++ components/CollapsibleArea/CollapsibleArea.tsx
@@ -1,11 +1,35 @@
-import React, {ReactNode} from "react";
+import {ReactNode, useEffect, useRef, useState} from "react";
 import styles from "./CollapsibleArea.module.scss";
 
-function CollapsibleArea({children, collapsed = false}: {children?: ReactNode; collapsed?: boolean}) {
+function CollapsibleArea({children, collapsed = true}: {children?: ReactNode; collapsed?: boolean}) {
+	const div = useRef<HTMLDivElement>(null);
+	const [scrollHeight, setScrollHeight] = useState<string | number>(445);
+
+	// set initial scroll height
+	useEffect(() => {
+		if (div.current !== null) {
+			setScrollHeight(div.current.scrollHeight);
+		}
+	}, []);
+
+	// update scroll height when the window resizes
+	useEffect(() => {
+		function handleResize() {
+			if (div.current !== null) {
+				setScrollHeight(div.current.scrollHeight);
+			}
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
-		<div className={collapsed ? styles.collapsed : styles.expanded}>
+		<div style={{height: collapsed ? "" : scrollHeight}} className={collapsed ? styles.collapsed : styles.expanded}>
+			<div ref={div}>{children}</div>
 			<div className={styles.foreground}></div>
-			{children}
 		</div>
 	);
 }
