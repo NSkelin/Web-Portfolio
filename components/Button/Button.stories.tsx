ba71d774e74286225225a349ae8841cbb54da409
Nick <nick.skelin@gmail.com>
2023-06-15 16:20:05 -0700

    initial commit

---
+++ components/Button/Button.stories.tsx
@@ -0,0 +1,49 @@
+import {Meta, StoryObj} from "@storybook/react";
+import "../../pages/globalStyle.scss";
+import Button from "./Button";
+
+const meta: Meta<typeof Button> = {
+	component: Button,
+	args: {
+		children: "Button",
+	},
+	argTypes: {
+		onClick: {
+			control: false,
+		},
+		children: {
+			control: false,
+		},
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const FAB: Story = {
+	args: {
+		style: "fab",
+	},
+};
+export const Filled: Story = {
+	args: {
+		style: "filled",
+	},
+};
+export const Outlined: Story = {
+	args: {
+		style: "outlined",
+	},
+	parameters: {
+		backgrounds: {
+			default: "dark gray",
+			values: [{name: "dark gray", value: "#4b4b4b"}],
+		},
+	},
+};
+export const IconButton: Story = {
+	args: {
+		style: "iconButton",
+		children: <span className="material-symbols-outlined">menu</span>,
+	},
+};
