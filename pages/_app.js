6332f837bab2ecbe9feba55d2a4434b2fd455f25
Nick <nick.skelin@gmail.com>
2023-06-20 15:20:32 -0700

    Add font using next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "./globalStyle.scss";
+import {Aleo} from "next/font/google";
+
+const aleo = Aleo({
+	subsets: ["latin"],
+	weight: "400",
+});
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={aleo.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
 export default MyApp;
