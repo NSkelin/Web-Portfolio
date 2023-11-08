f6ee3399195b44094f6553924a1c477eeeadad7d
Nick <nick.skelin@gmail.com>
2023-11-08 15:14:10 -0800

    throw error on invalid heading level

--- components/AccentTitle/AccentTitle.tsx
+++ components/AccentTitle/AccentTitle.tsx
@@ -18,7 +18,12 @@ interface AccentTitleProps {
  * Renders a title with a horizontal bar underneath for sectioning different types of content.
  */
 function AccentTitle({title = "Title", headingLevel = 2}: AccentTitleProps) {
-	const HTag = `h${headingLevel < 1 || headingLevel > 6 ? 1 : headingLevel}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+	// Throw an error if the heading level is outside the heading range.
+	if (headingLevel < 1 || headingLevel > 6) {
+		throw new Error(`Error: Prop "headingLevel" outside of range! Must be from 1 - 6. Is: ${headingLevel}`);
+	}
+
+	const HTag = `h${headingLevel}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 	return (
 		<div className={styles.container}>
 			<div className={styles.titleWrapper}>
