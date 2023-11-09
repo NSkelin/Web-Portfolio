110d0faa52cc1fc6260e0da671ce6fe3577fdc4c
Nick <nick.skelin@gmail.com>
2023-11-09 15:20:00 -0800

    add documentation

--- components/HyperLink/HyperLink.tsx
+++ components/HyperLink/HyperLink.tsx
@@ -2,10 +2,16 @@ import React, {ComponentProps} from "react";
 import styles from "./HyperLink.module.css";
 
 export interface HyperLinkProps {
+	/** The text that will be a clickable hyperlink. */
 	text?: string;
+	/** The icon to place next to the hyperlink. */
 	Icon: React.ComponentType<ComponentProps<"svg">>;
+	/** The destination clicking the link leads to. */
 	href: string;
 }
+/**
+ * Renders a hyperlink text with an icon next to it.
+ */
 function HyperLink({text = "link", Icon, href}: HyperLinkProps) {
 	return (
 		<div className={styles.container}>
