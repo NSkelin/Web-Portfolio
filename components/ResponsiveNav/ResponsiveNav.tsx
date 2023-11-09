e287831f816648b766b921f456c68e1097c3ca0d
Nick <nick.skelin@gmail.com>
2023-11-08 16:27:51 -0800

    improve documentation

--- components/ResponsiveNav/ResponsiveNav.tsx
+++ components/ResponsiveNav/ResponsiveNav.tsx
@@ -4,15 +4,23 @@ import Button from "../Button/Button";
 import styles from "./ResponsiveNav.module.css";
 
 interface ResponsiveNavProps {
+	/** The elements to display inside the navigation menu. */
 	links: React.JSX.Element[];
 }
+/**
+ * Renders a navigation menu that changes depending on the screen width.
+ *
+ * With small screens (phone / tablet) the navigation is a dropdown menu with a hamburger button.
+ * On large screens (desktop) the navigation is a horizontal bar.
+ */
 function ResponsiveNav({links}: ResponsiveNavProps) {
 	const [menuIcon, setMenuIcon] = useState("menu");
 	const [navDisplay, setNavDisplay] = useState("none");
 	const navRef = useRef<HTMLElement>(null);
 
-	// changes the menu icon to closed when a click happens outside of the nav element
+	// Collapses the navigation menu when the user clicks / taps outside the specified area.
 	useEffect(() => {
+		// Close navigation menu.
 		function handleClickOutside(event: MouseEvent) {
 			if (navRef.current && !navRef.current.contains(event.target as Node)) {
 				if (menuIcon != "menu") {
@@ -21,19 +29,23 @@ function ResponsiveNav({links}: ResponsiveNavProps) {
 				}
 			}
 		}
-		// Bind the event listener
+		// Bind the event listener.
 		document.addEventListener("mousedown", handleClickOutside);
 		return () => {
-			// Unbind the event listener on clean up
+			// Unbind the event listener on clean up.
 			document.removeEventListener("mousedown", handleClickOutside);
 		};
 	}, [menuIcon]);
 
+	/**
+	 * Expands the navigation menu.
+	 */
 	function toggleMenu() {
 		setMenuIcon(menuIcon === "menu" ? "menu_open" : "menu");
 		setNavDisplay(navDisplay === "none" ? "flex" : "none");
 	}
 
+	// Create the list of items inside the navigation menu.
 	const list = links.map((link, index) => {
 		return link === null ? null : (
 			<li key={index} className={styles.navItem}>
@@ -42,6 +54,7 @@ function ResponsiveNav({links}: ResponsiveNavProps) {
 		);
 	});
 
+	// Sets the nav button icon based on the navs state (expanded / collapsed).
 	const icon =
 		menuIcon === "menu" ? <Menu className={styles.materialSymbol} /> : <MenuOpen className={styles.materialSymbol} />;
 
