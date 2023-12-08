1f80367429f648451a53e262d8a4f2ccf5bf8214
Nick <nick.skelin@gmail.com>
2023-12-08 15:05:23 -0800

    Refactor link button creation

--- src/components/ProjectCardHeader/ProjectCardHeader.tsx
+++ src/components/ProjectCardHeader/ProjectCardHeader.tsx
@@ -1,5 +1,5 @@
 import {GithubMark, OpenInNew} from "public/icons";
-import React from "react";
+import React, {ComponentProps} from "react";
 import ResponsiveNav from "../ResponsiveNav";
 import styles from "./ProjectCardHeader.module.scss";
 
@@ -27,34 +27,28 @@ export interface ProjectCardHeaderProps {
  * The header contains the projects title and links to further resources such as the source code and live sites running the project.
  */
 function ProjectCardHeader({title, githubRepoURL, liveSiteURL, demoSiteURL}: ProjectCardHeaderProps) {
-	// Create links to specific sources for the project if they exist.
-	// Link to project github page.
-	const github = githubRepoURL ? (
-		<a className={styles.outline} href={githubRepoURL}>
-			<div className={styles.stateLayer}>
-				<GithubMark className={styles.materialSymbol} />
-				Github
-			</div>
-		</a>
-	) : null;
-	// Link to projects live site.
-	const view = liveSiteURL ? (
-		<a className={styles.fill} href={liveSiteURL}>
-			<div className={styles.stateLayer}>
-				<OpenInNew className={styles.materialSymbol} />
-				Live
-			</div>
-		</a>
-	) : null;
-	// Link to a site containing a demo version of the project.
-	const demo = demoSiteURL ? (
-		<a className={styles.outline} href={demoSiteURL}>
-			<div className={styles.stateLayer}>
-				<OpenInNew className={styles.materialSymbol} />
-				Demo
-			</div>
-		</a>
-	) : null;
+	/**
+	 * Creates a link button for the header.
+	 *
+	 * @param style The style to apply to the link button.
+	 * @param href The URL to link to.
+	 * @param text The text to display on the link button.
+	 * @param Icon The icon to display on the link button.
+	 */
+	function createLinkButton(style: string, href: string, text: string, Icon: React.ComponentType<ComponentProps<"svg">>) {
+		return (
+			<a className={style} href={href}>
+				<div className={styles.stateLayer}>
+					<Icon className={styles.materialSymbol} />
+					{text}
+				</div>
+			</a>
+		);
+	}
+
+	const github = githubRepoURL ? createLinkButton(styles.outline, githubRepoURL, "GitHub", GithubMark) : null;
+	const view = liveSiteURL ? createLinkButton(styles.fill, liveSiteURL, "Live", OpenInNew) : null;
+	const demo = demoSiteURL ? createLinkButton(styles.outline, demoSiteURL, "Demo", OpenInNew) : null;
 
 	return (
 		<header className={styles.header}>
