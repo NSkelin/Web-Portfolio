b166084e4b1559a822e3048c18152abd372825a2
Nick <nick.skelin@gmail.com>
2024-02-05 14:56:35 -0800

    add default story

---
+++ src/components/ContactForm/ContactForm.stories.tsx
@@ -0,0 +1,12 @@
+import {Meta, StoryObj} from "@storybook/react";
+
+import ContactForm from "./ContactForm";
+
+const meta: Meta<typeof ContactForm> = {
+	component: ContactForm,
+};
+
+export default meta;
+type Story = StoryObj<typeof ContactForm>;
+
+export const Default: Story = {};
